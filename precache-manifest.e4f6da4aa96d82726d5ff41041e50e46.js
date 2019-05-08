115d0c32380ab667c46869a0596948dbf3846b04
IgnasDev <ignas.rocys.dev@gmail.com>
2019-05-08 15:37:40 +0300

    Updates

---
+++ precache-manifest.e4f6da4aa96d82726d5ff41041e50e46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55aaba8411f003283f48889ce19e8ea",
+    "url": "/REACT-robofriends/index.html"
+  },
+  {
+    "revision": "8fce99e53cdaa2088f7f",
+    "url": "/REACT-robofriends/static/css/main.3b8e8927.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/REACT-robofriends/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8fce99e53cdaa2088f7f",
+    "url": "/REACT-robofriends/static/js/main.974f3ec4.chunk.js"
+  },
+  {
+    "revision": "d7fb7afd3f4827a845d7",
+    "url": "/REACT-robofriends/static/js/runtime~main.bf89f9d5.js"
+  }
+]);
